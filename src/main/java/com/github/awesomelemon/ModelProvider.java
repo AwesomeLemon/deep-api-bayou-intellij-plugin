246ccb4b6f1c230870ef2773bfc0f864633c8110
Alexander Chebykin <a.e.chebykin@gmail.com>
2018-05-01 12:16:20 +0300

    Implemented model download

---
+++ src/main/java/com/github/awesomelemon/ModelProvider.java
@@ -0,0 +1,135 @@
+package com.github.awesomelemon;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Locale;
+
+public class ModelProvider {
+    private final String tmpDir = System.getProperty("java.io.tmpdir");
+    private final Path pluginTmpDir = Paths.get(tmpDir, "deep_api_plugin");
+    private final String modelUrl = "https://www.dropbox.com/s/qh4kh6yy7d4jm2q/deep-api-tensorflow-modelA.zip?dl=1";
+    final String zipName = "model.zip";
+    private final String beamOpsLinux = "_beam_search_ops_linux.so";
+    private final String beamOpsMacos = "_beam_search_ops_macos.so";
+    private final String beamOpsWindows = "_beam_search_ops.dll";
+    private final String exportedModelDir = "deep-api-model";
+
+    public ModelProvider() {
+        try {
+            OsCheck.OSType osType = OsCheck.getOperatingSystemType();
+            if (!(osType.equals(OsCheck.OSType.Linux) || osType.equals(OsCheck.OSType.Windows))) {
+                throw new RuntimeException("Only Windows and Linux are supported right now");
+            }
+            if (!pluginTmpDir.toFile().exists()) {
+                pluginTmpDir.toFile().mkdir();
+                Path path = downloadTo(new URL(modelUrl), Paths.get(pluginTmpDir.toString(), zipName));
+                extractSubDir(path, pluginTmpDir);
+            } else {
+                Path modelDir = pluginTmpDir.resolve(exportedModelDir);
+
+                String beamOps = null;
+                if (osType.equals(OsCheck.OSType.Linux)) beamOps = beamOpsLinux;
+                if (osType.equals(OsCheck.OSType.Windows)) beamOps = beamOpsWindows;
+                Path beamOpPath = pluginTmpDir.resolve(beamOps);
+
+                if (!Files.exists(modelDir) || !Files.exists(beamOpPath)) {
+                    Path zipPath = pluginTmpDir.resolve(zipName);
+                    if (!Files.exists(zipPath)) {
+                        zipPath = downloadTo(new URL(modelUrl), Paths.get(pluginTmpDir.toString(), zipName));
+                    }
+                    extractSubDir(zipPath, pluginTmpDir);
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void extractSubDir(Path zipFilePath, Path targetDir) throws IOException {
+        FileSystem zipFs = FileSystems.newFileSystem(zipFilePath, null);
+        Path pathInZip = zipFs.getPath("/");
+        Files.walkFileTree(pathInZip, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path filePath, BasicFileAttributes attrs) throws IOException {
+                // Make sure that we conserve the hierachy of files and folders inside the zip
+                Path relativePathInZip = pathInZip.relativize(filePath);
+                Path targetPath = targetDir.resolve(relativePathInZip.toString());
+                Files.createDirectories(targetPath.getParent());
+
+                // And extract the file
+                Files.copy(filePath, targetPath);
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    public String getBeamOpsPath() {
+        String beamOps = null;
+        if (OsCheck.getOperatingSystemType().equals(OsCheck.OSType.Linux)) beamOps = beamOpsLinux;
+        else beamOps = beamOpsWindows;
+        return Paths.get(pluginTmpDir.toString(), beamOps).toString();
+    }
+
+    public String getExportedModelPath() {
+        return Paths.get(pluginTmpDir.toString(), exportedModelDir).toString();
+    }
+
+    private Path downloadTo(URL url, Path path) throws IOException {
+        path.toFile().getParentFile().mkdirs();
+
+        URLConnection conn = url.openConnection();
+        int contentLength = conn.getContentLength();
+
+        InputStream in = url.openStream();
+        try (BufferedInputStream bis = new BufferedInputStream(in)) {
+            FileOutputStream out = new FileOutputStream(path.toFile());
+            byte[] data = new byte[1024];
+            int totalCount = 0;
+            int count = bis.read(data, 0, 1024);
+            while (count != -1) {
+                out.write(data, 0, count);
+                count = bis.read(data, 0, 1024);
+            }
+
+        }
+
+        return path;
+    }
+
+    private final static class OsCheck {
+        public enum OSType {
+            Windows, MacOS, Linux, Other
+        };
+
+        static OSType detectedOS;
+
+        public static OSType getOperatingSystemType() {
+            if (detectedOS == null) {
+                String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+                if ((OS.contains("mac")) || (OS.contains("darwin"))) {
+                    detectedOS = OSType.MacOS;
+                } else if (OS.contains("win")) {
+                    detectedOS = OSType.Windows;
+                } else if (OS.contains("nux")) {
+                    detectedOS = OSType.Linux;
+                } else {
+                    detectedOS = OSType.Other;
+                }
+            }
+            return detectedOS;
+        }
+    }
+
+//    public static void main(String[] args) throws IOException {
+//        ModelProvider modelProvider = new ModelProvider();
+//    }
+}
