c03473256a36daee167b22d235e563d4bc5ddc1b
Alexander Chebykin <a.e.chebykin@gmail.com>
2018-05-01 12:17:23 +0300

    Try to make ProgressIndicator work

--- src/main/java/com/github/awesomelemon/GenerateIntentionAction.java
+++ src/main/java/com/github/awesomelemon/GenerateIntentionAction.java
@@ -1,11 +1,15 @@
 package com.github.awesomelemon;
 
 import com.github.awesomelemon.Bayou.BayouModelFacade;
+import com.github.awesomelemon.DeepAPI.ApiCallSequence;
+import com.github.awesomelemon.DeepAPI.DeepApiModelFacade;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
 import com.intellij.formatting.WhiteSpace;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -14,7 +18,9 @@ import org.jetbrains.annotations.*;
 
 public class GenerateIntentionAction extends PsiElementBaseIntentionAction implements IntentionAction {
 
-    private static final String INTENTION_PREFIX = "///";
+    private static final String INTENTION_PREFIX = "//";
+    private BayouModelFacade bayouModelFacade;
+    private DeepApiModelFacade deepApiModelFacade;
 
     @NotNull
     public String getText() {
@@ -69,72 +75,40 @@ public class GenerateIntentionAction extends PsiElementBaseIntentionAction imple
     }
 
     public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
+        ProgressManager.getInstance().run(new Task.Backgroundable(project, "daf") {
+            public void run(ProgressIndicator indicator) {
+                indicator.setText("This is how you update the indicator");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                indicator.setFraction(0.5);  // halfway done
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         PsiComment comment = asComment(element);
         String request = comment.getText().substring(INTENTION_PREFIX.length());
         System.out.println(request);
-        BayouModelFacade bayouModelFacade = new BayouModelFacade(project);
-        DeepApiModelFacade deepApiModelFacade = new DeepApiModelFacade();
+//        if (this.bayouModelFacade == null || !this.bayouModelFacade.getProject().equals(project)) {
+//        }
+        if (this.deepApiModelFacade == null) {
+            this.deepApiModelFacade = new DeepApiModelFacade();
+        }
         ApiCallSequence bayouInput = deepApiModelFacade.generateBayouInput(request);
         System.out.println(bayouInput.getApiMethods());
         System.out.println(bayouInput.getApiTypes());
-        ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> bayouModelFacade.generate(bayouInput, getContainingMethod(comment)),
-                "dsjall", true, project);
-//        final int offset = editor.getCaretModel().getOffset();
-//        PsiConditionalExpression conditionalExpression = PsiTreeUtil.getParentOfType(element,
-//                PsiConditionalExpression.class, false);
-//        if (conditionalExpression == null) return;
-//        if (conditionalExpression.getThenExpression() == null || conditionalExpression.getElseExpression() == null) return;
-//
-//        final PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
-//
-//        PsiElement originalStatement = PsiTreeUtil.getParentOfType(conditionalExpression, PsiStatement.class, false);
-//        while (originalStatement instanceof PsiForStatement) {
-//            originalStatement = PsiTreeUtil.getParentOfType(originalStatement, PsiStatement.class, true);
-//        }
-//        if (originalStatement == null) return;
-//
-//        // Maintain declrations
-//        if (originalStatement instanceof PsiDeclarationStatement) {
-//            final PsiDeclarationStatement declaration = (PsiDeclarationStatement) originalStatement;
-//            final PsiElement[] declaredElements = declaration.getDeclaredElements();
-//            PsiLocalVariable variable = null;
-//            for (PsiElement declaredElement : declaredElements) {
-//                if (declaredElement instanceof PsiLocalVariable &&
-//                        PsiTreeUtil.isAncestor(declaredElement, conditionalExpression, true)) {
-//                    variable = (PsiLocalVariable) declaredElement;
-//                    break;
-//                }
-//            }
-//            if (variable == null) return;
-//            variable.normalizeDeclaration();
-//            final Object marker = new Object();
-//            PsiTreeUtil.mark(conditionalExpression, marker);
-//            PsiExpressionStatement statement =
-//                    (PsiExpressionStatement) factory.createStatementFromText(variable.getName() + " = 0;", null);
-//            statement = (PsiExpressionStatement) CodeStyleManager.getInstance(project).reformat(statement);
-//            ((PsiAssignmentExpression) statement.getExpression()).getRExpression().replace(variable.getInitializer());
-//            variable.getInitializer().delete();
-//            final PsiElement variableParent = variable.getParent();
-//            originalStatement = variableParent.getParent().addAfter(statement, variableParent);
-//            conditionalExpression = (PsiConditionalExpression) PsiTreeUtil.releaseMark(originalStatement, marker);
-//        }
-//
-//        // create then and else branches
-//        final PsiElement[] originalElements = new PsiElement[]{originalStatement, conditionalExpression};
-//        final PsiExpression condition = (PsiExpression) conditionalExpression.getCondition().copy();
-//        final PsiElement[] thenElements = PsiTreeUtil.copyElements(originalElements);
-//        final PsiElement[] elseElements = PsiTreeUtil.copyElements(originalElements);
-//        thenElements[1].replace(conditionalExpression.getThenExpression());
-//        elseElements[1].replace(conditionalExpression.getElseExpression());
-
-//        PsiIfStatement statement = (PsiIfStatement) factory.createStatementFromText("if (true) { a = b } else { c = d }",
-//                null);
-//        statement = (PsiIfStatement) CodeStyleManager.getInstance(project).reformat(statement);
-//        statement.getCondition().replace(condition);
-//        statement = (PsiIfStatement) originalStatement.replace(statement);
-//
-//        ((PsiBlockStatement) statement.getThenBranch()).getCodeBlock().getStatements()[0].replace(thenElements[0]);
-//        ((PsiBlockStatement) statement.getElseBranch()).getCodeBlock().getStatements()[0].replace(elseElements[0]);
+        ProgressManager instance = ProgressManager.getInstance();
+        instance.run(new Task.Modal(project, "Abc", true) {
+            @Override
+            public void run(@NotNull ProgressIndicator indicator) {
+                new BayouModelFacade(project, bayouInput, getContainingMethod(comment)).run();
+            }
+        });
         System.out.println("done!");
     }
 
--- src/main/java/com/github/awesomelemon/HelloAction.java
+++ src/main/java/com/github/awesomelemon/HelloAction.java
@@ -1,6 +1,9 @@
 package com.github.awesomelemon;
 
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 
@@ -10,7 +13,19 @@ public class HelloAction extends AnAction {
     }
 
     public void actionPerformed(AnActionEvent event) {
-        Project project = event.getProject();
+        Project project = event.getData(CommonDataKeys.PROJECT);
+        ProgressManager.getInstance().run(new Task.Modal(project, "daf", false) {
+            public void run(ProgressIndicator indicator) {
+                indicator.setText("This is how you update the indicator");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {}
+                indicator.setFraction(0.5);  // halfway done
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {}
+            }
+        });
         Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
     }
 }
